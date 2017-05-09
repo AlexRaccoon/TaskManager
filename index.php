e498a3908517072189d54d9e27515bcca670e045
AlexRaccoon <alexeclipse-github@yahoo.fr>
2017-05-09 12:21:17 +0200

    Premier commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wap34
+ * Date: 05/05/17
+ * Time: 15:52
+ */
+require "./src/utilities.php";
+
+$filename = "./tasks.csv";
+$taskList = loadTask($filename);
+require  "./views/index.html";
+// CECI est un TEST
